b7370d10b0f100a3a12be58fc91f0d52f031071c
soofm <victorlee228@gmail.com>
2020-06-26 20:51:19 -0400

    Added check for WoW installation directory

--- src/main/start.ts
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-
-async function glob (dir: string, paths: string[] = []) {
-  const files = await fs.readdir(dir)
-  
-}
--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,15 +1,39 @@
 /* eslint-disable no-unused-vars */
-const { remote } = require('electron')
+const fs = require('fs')
+const path = require('path')
+const fg = require('fast-glob')
 
 const installDirEl = document.getElementById('install-dir')
 
 async function selectInstallDir () {
+  const { remote } = require('electron')
   const dialog = remote.dialog
   const dialogResult = await dialog.showOpenDialog({
     properties: ['openDirectory']
   })
   if (!dialogResult.canceled) {
     const installDir = dialogResult.filePaths[0]
-    installDirEl.innerText = installDir
+    if (await checkInstallDir(installDir)) {
+      installDirEl.innerText = installDir
+      installDirEl.classList.add('show')
+    }
   }
+}
+
+async function checkInstallDir (installDir: string) {
+  const options = { cwd: installDir }
+  try {
+    const [entries, addons, wtf] = await Promise.all([
+      fg(['World*.app', 'Wow*.exe'], options),
+      fs.promises.stat(path.resolve(installDir, 'Interface/AddOns')),
+      fs.promises.stat(path.resolve(installDir, 'WTF'))
+    ])
+
+    if (entries.length === 0 || !addons.isDirectory() || !wtf.isDirectory()) {
+      throw new Error(`${entries.length}---${addons.isDirectory()}---${wtf.isDirectory()}`)
+    }
+  } catch (err) {
+    alert('World of Warcraft installation not found.' + err)
+  }
+  return true
 }
